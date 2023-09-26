6135c731a1308edb0700049e6238cfcb57957fb6
Tasanai Srisawat <tasanai.srisawat@gmail.com>
2023-09-26 15:59:16 +0700

    feat: update preview

--- backend/src/wss.py
+++ backend/src/wss.py
@@ -1,9 +1,16 @@
 import asyncio
 import websockets
 
+from datetime import datetime
+
 async def echo(websocket):
     async for message in websocket:
-        await websocket.send(message)
+        # Send every 1 seconds 3 times
+        await websocket.send(f'{message.upper()} @{datetime.now()}')
+        await asyncio.sleep(1)
+        await websocket.send(f'{message.upper()[::-1]} @{datetime.now()}')
+        await asyncio.sleep(1)
+        await websocket.send(f'Length: {len(message)} @{datetime.now()}')
 
 async def main():
     async with websockets.serve(echo, host="", port=80):
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,18 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <HelloWorld />
+  <h1 class="mb-12">Vue + Python Websockets</h1>
+  <div>
+    <input v-model="message" type="text" class="p-3 mr-3 rounded-lg" />
+    <button @click="send">Send</button>
+  </div>
+  <div class="mt-6 grid grid-cols-3 gap-3">
+    <div
+      v-for="msg in recievedMessages"
+      class="p-3 bg-gray-100 text-slate-900 rounded-lg"
+    >
+      {{ msg }}
+    </div>
+  </div>
 </template>
-
 <style scoped>
 .logo {
   height: 6em;
@@ -20,3 +27,19 @@ import HelloWorld from './components/HelloWorld.vue'
   filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
+<script setup lang="ts">
+import { ref } from "vue";
+
+const websocket = new WebSocket("wss://playground.tanssw.com/wss/socket");
+
+const message = ref("");
+const recievedMessages = ref<string[]>([]);
+function send() {
+  websocket.send(message.value);
+  message.value = "";
+}
+
+websocket.onmessage = (event) => {
+  recievedMessages.value.push(event.data as string);
+};
+</script>
